23f43b2033645d1ede170969720e8572233538a7
bachatron <idz420@gmail.com>
2022-09-21 23:57:51 -0300

    Added sum and result function.

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const display = document.getElementById("display");
+const plus = document.getElementById("plus")
 
-let firstNumber = ""
+let displayNumber = "";
+let storedNumber = "";
+let storedOperator = "";
 
 
 
 const addNumber = function(num) {
-    firstNumber += num;
-    display.innerText = firstNumber;
-    console.log(firstNumber);
+    displayNumber += num;
+    display.innerText = displayNumber;
+    console.log(displayNumber);
 };
 
 const deleteNumber = function(){
-    firstNumber = firstNumber.slice(0, -1);
-    display.innerText = firstNumber;
-    console.log(firstNumber);
+    displayNumber = displayNumber.slice(0, -1);
+    display.innerText = displayNumber;
+    console.log(displayNumber);
+}
+
+const add = (n1, n2) => n1 + n2;
+const substract = (n1, n2) => n1 - n2;
+const multiply = (n1, n2) => n1 * n2;
+const divide = (n1, n2) => n1 / n2;
+
+const setOperator = (op) => storedOperator = op; 
+
+const operate = function (n1, n2, operator){
+    storedOperator !== "" ? displayNumber = operator(n1, n2) : undefined;
 }
