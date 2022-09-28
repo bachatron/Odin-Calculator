e14c4332d76091587bae192b7c148ca1cf47b8a3
bachatron <idz420@gmail.com>
2022-09-28 20:05:49 -0300

    Now can add operations without using the equal button

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const display = document.getElementById("display");
 const plus = document.getElementById("plus")
-const operatorDisplay = document.getElementById("operatorDisplay");
-const storedDisplay = document.getElementById("storedDisplay");
-
 
 let displayNumber = "";
 let storedNumber = "";
@@ -10,28 +7,27 @@ let storedOperator = "";
 
 const updateDisplay = () => {
     display.innerText = displayNumber;
-    storedDisplay.innerText = storedNumber;
-    operatorDisplay.innerText = storedOperator;
 };
+
 
 //ADD NUMBER TO THE DISPLAY
 const addNumber = function(num) {
     displayNumber += num;
-   updateDisplay()
-    console.log(displayNumber);
+    updateDisplay();
+    //console.log(displayNumber);
 }
 
 //DELETE LAST NUMBER FROM DISPLAY
 const deleteNumber = function(){
     displayNumber = displayNumber.slice(0, -1);
-   updateDisplay()
-    console.log(displayNumber);
+    updateDisplay()
+    //console.log(displayNumber);
 }
 
 //RESET DISPLAY AND AL STORED VALUES
 const clearDisplay = () => {
     displayNumber = storedNumber = storedOperator = "";
-   updateDisplay()
+    updateDisplay()
 }
 
 //SUM SUBSTRACT MULTIPLY AND DIVIDE FUNCTIONS
@@ -42,18 +38,23 @@ const divide = (n1, n2) => n1 / n2;
 
 //STORE THE SELECTED OPERATOR
 const setOperator = (op) => {
+    if (storedNumber !== ""){
+        operate(storedNumber, displayNumber, storedOperator);
+    }
     storedOperator = op;
     storedNumber = displayNumber;
     displayNumber = "";
+    console.log(`DN: ${displayNumber} SN: ${storedNumber}`)
 }
 
 const operate = function (n1, n2, operator){
-    if (storedOperator == "" || displayNumber == "" || storedNumber == "") {
+    if (displayNumber == "" || storedNumber == "" || storedOperator == ""){
         return;
-    } else {
-        displayNumber = String((operator(parseInt(n1), parseInt(n2))));
-        storedNumber = "";
+    } else{
+        displayNumber = String((operator(parseFloat(n1), parseFloat(n2))));
+        updateDisplay();
     }
-    updateDisplay()
-    console.log(displayNumber);
+    storedOperator = "";
+
+    
 }
